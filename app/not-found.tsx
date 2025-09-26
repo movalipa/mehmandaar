e71464f88ef821502bdea44fbb9d30597b2e9ffe
movalipa <movalipa@gmail.com>
2025-09-26 15:03:01 +0330

    ✨ initial boilerplate configed

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <main className="flex h-full items-center justify-center text-gray-700">
+      <h3 className="text-2xl font-medium">❌ صفحه مورد نظر یافت نشد</h3>
+    </main>
+  )
+}
+
+export default NotFound
