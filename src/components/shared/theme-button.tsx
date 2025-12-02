ec6fe8ceee8fdacb67cb8a745a7619c6b146d0fc
movalipa <movalipa@gmail.com>
2025-12-02 14:56:58 +0330

    🐛theme toggle button hydration bug fixed

--- src/components/shared/theme-button.tsx
+++ src/components/shared/theme-button.tsx
@@ -2,11 +2,19 @@
 
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { useIsMounted } from "@/hooks/use-is-mounted"
 import { Button, type ButtonProps } from "../ui/button"
+import { Skeleton } from "../ui/skeleton"
 
 const ThemeButton = (props: ButtonProps) => {
   const { resolvedTheme, setTheme } = useTheme()
   const isLight = resolvedTheme === "light"
+
+  // this fixes the hydration error
+  const isMounted = useIsMounted()
+  if (!isMounted) {
+    return <Skeleton className="w-9 h-9" />
+  }
 
   return (
     <Button
