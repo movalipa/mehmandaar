c653d5743bfa1ba8c7aae8f5f44458a395a4f8f7
movalipa <movalipa@gmail.com>
2025-12-04 15:08:21 +0330

    🚚drizzle path name updated

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from "~/env"
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./db/schema.ts",
+  schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
